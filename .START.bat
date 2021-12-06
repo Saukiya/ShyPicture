c2babbe915c3f7668af12a7fe1d2d65d8f251028
Saukiya <Saukiya@qq.com>
2021-12-06 11:16:47 +0800

    [perf] 添加可执行文件

---
+++ .START.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar ClassifiedPicture.jar
+pause
--- src/github/saukiya/www/Main.java
+++ src/github/saukiya/www/Main.java
@@ -1,16 +1,19 @@
 package github.saukiya.www;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_");
+        SimpleDateFormat sdfNormal = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+        List<String> whiteList = Arrays.asList("out", "src", ".idea", ".git", ".gitignore", "ShyPicture.iml", "ClassifiedPicture.jar", ".START.bat");
         File parentFile = new File("./");
         List<String> lockList = new ArrayList<>(Arrays.stream(parentFile.listFiles()).filter(file -> file.getName().startsWith("IMG_")).map(file -> file.getName().substring(4,file.getName().lastIndexOf("."))).toList());
         List<File> fileList = Arrays.stream(parentFile.listFiles())
-                .filter(file -> !file.isDirectory() && !file.getName().endsWith(".iml") && !file.getName().startsWith("IMG_"))
+                .filter(file -> !whiteList.contains(file.getName()) && !file.getName().startsWith("IMG_"))
                 .sorted(new FileComparator()).toList();
         for (File file : fileList) {
             String fileName = file.getName();
@@ -21,7 +24,7 @@ public class Main {
                 index++;
             }
             lockList.add(prefix+index);
-            System.out.println("RENAME: " + prefix + index + suffix + "\t" + file.length());
+            System.out.println("RENAME: " + prefix + index + suffix + '\t' + sdfNormal.format(file.lastModified()) + '\t' + file.length());
             file.renameTo(new File(parentFile, "IMG_" + prefix + index + suffix));
         }
     }
